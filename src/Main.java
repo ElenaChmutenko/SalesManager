c462bfe79497ce7fa19cfe4a595076b96d204dae
chmutenkoa <alenachm@gmail.com>
2022-07-27 16:06:14 +0300

    Int changed to long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = { 2, 3, 9, 0, 18, 1, 7 };
+        long[] sales = { 2, 3, 9, 0, 19, 1, 7 };
         SalesManager manager = new SalesManager(sales);
         System.out.println(manager.max());
     }
